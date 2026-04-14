98f15498daa92bcd227ca7da16ad517df7636d29
Manu v <mokshithv703@gmail.com>
2026-04-14 19:39:49 +0530

    Update PolyalphabeticCipher.java

--- PolyalphabeticCipher.java
+++ PolyalphabeticCipher.java
@@ -1,19 +1,30 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class PolyalphabeticCipher {
 
-    static String process(String text, String key, boolean enc) {
+    static String process(String text, String key, boolean encrypt) {
         StringBuilder res = new StringBuilder();
+
         text = text.toUpperCase();
         key = key.toUpperCase();
 
-        for (int i = 0, j = 0; i < text.length(); i++) {
-            char c = text.charAt(i);
+        int j = 0; // key index
 
-            if (Character.isLetter(c)) {
-                int shift = key.charAt(j++ % key.length()) - 'A';
-                res.append((char) ((c - 'A' + (enc ? shift : -shift) + 26) % 26 + 'A'));
-            } else res.append(c);
+        for (char ch : text.toCharArray()) {
+            if (Character.isLetter(ch)) {
+
+                int shift = key.charAt(j % key.length()) - 'A';
+
+                if (!encrypt) {
+                    shift = 26 - shift; // reverse shift for decryption
+                }
+
+                res.append((char) ((ch - 'A' + shift) % 26 + 'A'));
+                j++;
+
+            } else {
+                res.append(ch);
+            }
         }
         return res.toString();
     }
@@ -21,13 +32,14 @@ public class PolyalphabeticCipher {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter Plain Text: ");
+        System.out.print("Enter text: ");
         String text = sc.nextLine();
 
-        System.out.print("Enter Key: ");
+        System.out.print("Enter key: ");
         String key = sc.nextLine();
 
         String enc = process(text, key, true);
+
         System.out.println("Encrypted Text: " + enc);
         System.out.println("Decrypted Text: " + process(enc, key, false));
     }
